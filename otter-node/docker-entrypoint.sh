c9458009741fb36ce7b70ae220377ba35296b79c
blademainer <blademainer@gmail.com>
2019-06-10 22:11:57 +0800

    update manager

--- otter-node/docker-entrypoint.sh
+++ otter-node/docker-entrypoint.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+_OTTER_MANAGER_ADDRESS=""
+_MYSQL_PORT="3306"
+
+envs="OTTER_MANAGER_ADDRESS
+MSYQL_HOST
+MYSQL_USER
+MYSQL_PASSWORD
+MYSQL_PORT
+"
+
+while read arg; do
+    if [ "$(eval echo \$$arg)"x == ""x ]; then
+        echo "env: $(eval echo $arg) could'nt be null!"
+        exit 1
+    fi
+done <<< "$envs"
+
