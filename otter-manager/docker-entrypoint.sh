62dbe9044b26177a2433b443f464a4ee37670818
blademainer <blademainer@gmail.com>
2019-07-11 20:35:20 +0800

    add test file

--- otter-manager/docker-entrypoint.sh
+++ otter-manager/docker-entrypoint.sh
@@ -4,8 +4,7 @@
 echo "optional env parameters:"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>"
 cat conf/otter.properties  | grep -E "^\w" | awk '{print $1}' | sed 's/\./_/g' | awk '{print "CONF_"$0}' | while read _opts; do
-    typeset -u _env_opts
-    _env_opts="$_opts"
+    _env_opts="`echo $_opts | tr [a-z] [A-Z]`"
     echo "$_env_opts"
 done
 echo "<<<<<<<<<<<<<<<<<<<<<<<<"
@@ -15,8 +14,7 @@ echo "<<<<<<<<<<<<<<<<<<<<<<<<"
 env | grep -E "^CONF_" | while read conf; do
     name=`echo "$conf" | awk -F "=" '{print $1}' | sed 's/CONF_//' | sed 's/_/\./g' `
     value=`echo "$conf" | awk -F "=" '{print $2}'`
-    typeset -l c
-    c="$name"
+    c="`echo $name | tr [A-Z] [a-z]`"
     echo "setting: $c = ${value}"
     sed -i "s/^${c}\s*=.*$/${c} = ${value}/g" conf/otter.properties
 #    sed -i "s/$c/$V"
